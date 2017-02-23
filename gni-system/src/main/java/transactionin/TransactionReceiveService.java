d67643bd17e72a8652713acdfa93eef3a21f5a98
Noel Keijzer <n.keijzer@student.utwente.nl>
2017-02-23 14:04:48 +0100

    Set up basic TransactionReceiveService layout

--- gni-system/src/main/java/transactionin/TransactionReceiveService.java
+++ gni-system/src/main/java/transactionin/TransactionReceiveService.java
@@ -1,8 +1,8 @@
 package transactionin;
 
-//import io.advantageous.qbit.annotation.Listen;
-import util.Transaction;
-//import manager.ServiceManager;
+import io.advantageous.qbit.annotation.RequestMapping;
+import io.advantageous.qbit.annotation.RequestParam;
+import io.advantageous.qbit.reactive.Callback;
 
 /**
  * @author Noel
@@ -10,28 +10,34 @@ import util.Transaction;
  * Receives transaction requests from external banks, send them to the ledger
  * for processing, and sends the confirmation/failure back to the external bank.
  */
+@RequestMapping("/transactionReceive")
 public class TransactionReceiveService {
-    //TODO setup socket to receive external traffic and parse it.
+    /**Used to verify if a http request to another service was successfull.*/
+    private static final int HTTP_OK = 200;
+    /**Port that the Ledger service can be found on.*/
+    private int ledgerPort;
+    /**Host that the User service can be found on.*/
+    private String ledgerHost;
 
     /**
-     * Receives transaction requests from external banks and sends them to the
-     * ledger for processing.
+     * Constructor.
+     * @param newLedgerPort Port the Ledger can be found on.
+     * @param newLedgerHost Host the ledger can be found on.
      */
-    //TODO rewrite for external json requests.
-    private void processIncomingTransaction() {
-        //TODO code to process transactions from other banks.
-        System.out.println("Processing transaction from an external bank.");
+    public TransactionReceiveService(final int newLedgerPort, final String newLedgerHost) {
+        this.ledgerPort = newLedgerPort;
+        this.ledgerHost = newLedgerHost;
     }
 
     /**
-     * Receives a transaction object back from the ledger then checks if it was
-     * successfull or failed and sends this back to the bank of origin.
-     * @param transaction Transaction that was processed by the ledger.
+     * Processes method coming from other banks, sends them to the ledger and then sends back a reply.
+     * @param callback Used to send a reply back to the external bank.
+     * @param body Json String representing an external transaction.
      */
-    //@Listen(ServiceManager.TRANSACTION_VERIFICATION_CHANNEL)
-    private void sendIncomingTransactionReply(final Transaction transaction) {
-        //TODO code to check whether a transaction was internal or external.
-        System.out.printf("Sent transaction reply to transaction number %s",
-                        transaction.getTransactionID());
+    //TODO might need reworking when it is clear how external transactions will be sent
+    public void processIncomingTransaction(final Callback<String> callback, final @RequestParam("body") String body) {
+        //TODO fill method
+
+        System.out.println("Processing transaction from an external bank.");
     }
 }
