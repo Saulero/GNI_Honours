63eb8db1bf3ac45c9d09860e2a5636df9c1a3b75
Noel Keijzer <n.keijzer@student.utwente.nl>
2017-04-27 22:05:20 +0200

    Add input sanitation for all UIService methods, untested

--- gni-system/src/main/java/pin/tableCreator.java
+++ gni-system/src/main/java/pin/tableCreator.java
@@ -10,6 +10,7 @@ import java.sql.SQLException;
 
 /**
  * @Author noel
+ * @version 1
  */
 public class tableCreator {
     private final static String createPinTable = "CREATE TABLE `pin` ( `user_id` int(11) NOT NULL, `card_number` varchar(255) NOT NULL, `pin_code` varchar(255) NOT NULL);";
@@ -17,8 +18,11 @@ public class tableCreator {
     public static void main(String[] args) {
         try {
             SQLConnection databaseConnection = databaseConnectionPool.getConnection();
+            System.out.println("x");
             databaseConnection.getConnection().prepareStatement(createPinTable).execute();
+            System.out.println("y");
             PreparedStatement addPin = databaseConnection.getConnection().prepareStatement(SQLStatements.addPinCard);
+            System.out.println("z");
             addPin.setInt(1, 1);
             addPin.setString(2, "123");
             addPin.setString(3, "0000");
