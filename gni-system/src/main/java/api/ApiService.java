e374ad738d8b26fc1406c6195258ebae2af39c0b
Noel Keijzer <n.keijzer@student.utwente.nl>
2017-06-04 16:40:33 +0200

    Refactor api handler, add clarifying comments to api methods.

--- gni-system/src/main/java/api/ApiService.java
+++ gni-system/src/main/java/api/ApiService.java
@@ -40,7 +40,7 @@ public final class ApiService {
         jsonConverter = new Gson();
     }
 
-    @RequestMapping(value = "/request")
+    @RequestMapping(value = "/request", method = RequestMethod.POST)
     public void handleApiRequest(final Callback<String> callback, final @RequestParam("request") String requestJson) {
         try {
             JSONRPC2Request request = JSONRPC2Request.parse(requestJson);
@@ -51,30 +51,30 @@ public final class ApiService {
             switch (method) {
                 case "openAccount":             openAccountHandler(params, callbackBuilder, id);
                     break;
-                /*case "openAdditionalAccount":   callback.reply(openAdditionalAccountHandler(params).toJSONString());
+                case "openAdditionalAccount":   openAdditionalAccountHandler(params);
                     break;
-                case "closeAccount":            callback.reply(closeAccountHandler(params).toJSONString());
+                case "closeAccount":            closeAccountHandler(params);
                     break;
-                case "provideAccess":           callback.reply(provideAccessHandler(params).toJSONString());
+                case "provideAccess":           provideAccessHandler(params);
                     break;
-                case "revokeAccess":            callback.reply(revokeAccessHandler(params).toJSONString());
+                case "revokeAccess":            revokeAccessHandler(params);
                     break;
-                case "depositIntoAccount":      callback.reply(depositIntoAccountHandler(params).toJSONString());
+                case "depositIntoAccount":      depositIntoAccountHandler(params);
                     break;
-                case "payFromAccount":          callback.reply(payFromAccountHandler(params).toJSONString());
+                case "payFromAccount":          payFromAccountHandler(params);
                     break;
-                case "transferMoney":           callback.reply(transferMoneyHandler(params).toJSONString());
+                case "transferMoney":           transferMoneyHandler(params);
                     break;
-                case "getAuthToken":            callback.reply(getAuthTokenHandler(params).toJSONString());
+                case "getAuthToken":            getAuthTokenHandler(params);
                     break;
-                case "getBalance":              callback.reply(getBalanceHandler(params).toJSONString());
+                case "getBalance":              getBalanceHandler(params);
                     break;
-                case "getTransactionsOverview": callback.reply(getTransactionsOverviewHandler(params).toJSONString());
+                case "getTransactionsOverview": getTransactionsOverviewHandler(params);
                     break;
-                case "getUserAccess":           callback.reply(getUserAccessHandler(params).toJSONString());
+                case "getUserAccess":           getUserAccessHandler(params);
                     break;
-                case "getBankAccountAccess":    callback.reply(getBankAccountAccessHandler(params).toJSONString());
-                    break;*/
+                case "getBankAccountAccess":    getBankAccountAccessHandler(params);
+                    break;
                 default:                        callback.reply(new JSONRPC2Response(JSONRPC2Error.METHOD_NOT_FOUND,
                         request.getID()).toJSONString());
                     break;
@@ -124,6 +124,7 @@ public final class ApiService {
                         doNewPinCardRequest(accountNumber, authenticationReply.getCookie(), callbackBuilder, id);
                     } else {
                         System.out.printf("%s Login failed.\n\n\n\n", PREFIX);
+                        //todo send failed reply
                     }
                 });
     }
@@ -140,6 +141,7 @@ public final class ApiService {
                                 newPinCard.getPinCode(), id);
                     } else {
                         System.out.printf("%s New pin card request failed.\n\n\n\n", PREFIX);
+                        //todo send failed reply
                     }
                 });
     }
@@ -154,16 +156,51 @@ public final class ApiService {
         callbackBuilder.build().reply(response.toJSONString());
     }
 
-    /*private JSONRPC2Response openAdditionalAccountHandler(final Map<String, Object> params) {}
-    private JSONRPC2Response closeAccountHandler(final Map<String, Object> params) {}
-    private JSONRPC2Response provideAccessHandler(final Map<String, Object> params) {}
-    private JSONRPC2Response revokeAccessHandler(final Map<String, Object> params) {}
-    private JSONRPC2Response depositIntoAccountHandler(final Map<String, Object> params) {}
-    private JSONRPC2Response payFromAccountHandler(final Map<String, Object> params) {}
-    private JSONRPC2Response transferMoneyHandler(final Map<String, Object> params) {}
-    private JSONRPC2Response getAuthTokenHandler(final Map<String, Object> params) {}
-    private JSONRPC2Response getBalanceHandler(final Map<String, Object> params) {}
-    private JSONRPC2Response getTransactionsOverviewHandler(final Map<String, Object> params) {}
-    private JSONRPC2Response getUserAccessHandler(final Map<String, Object> params) {}
-    private JSONRPC2Response getBankAccountAccessHandler(final Map<String, Object> params) {}*/
+    private void openAdditionalAccountHandler(final Map<String, Object> params) {
+        //todo needs modifications so users loads customerData during request as we currently require customer data to be specified.
+    }
+    private void closeAccountHandler(final Map<String, Object> params) {
+        uiClient.putFormAsyncWith2Params("/services/ui/account/remove", "accountNumber",
+                params.get("iBAN"), "cookie", params.get("authToken"), (code, contentType, body) -> {
+                    if (code == HTTP_OK) {
+                        System.out.printf("%s Successfully closed account %s\n\n\n\n", PREFIX, body);
+                        //todo handle response
+                    } else {
+                        System.out.printf("%s Account closing failed.\n\n\n\n", PREFIX);
+                    }
+                });
+    }
+    private void provideAccessHandler(final Map<String, Object> params) {
+        // does an account Link to a username(so we need a conversion for this internally)
+        // then performs a new pin card request for the customer with username.
+    }
+    private void revokeAccessHandler(final Map<String, Object> params) {
+        //todo add functionality for account Link removal
+        // performs an account Link removal and then removes the pincard(s) of said customer.
+        // look at documentation for more specifics.
+    }
+    private void depositIntoAccountHandler(final Map<String, Object> params) {
+        // can be implemented 1:1 with a pin transaction.
+    }
+    private void payFromAccountHandler(final Map<String, Object> params) {
+        // can be implemented 1:1 with a pin transaction.
+    }
+    private void transferMoneyHandler(final Map<String, Object> params) {
+        // can be implemented 1:1 with a normal transaction request.
+    }
+    private void getAuthTokenHandler(final Map<String, Object> params) {
+        // can be implemented 1:1 with do login.
+    }
+    private void getBalanceHandler(final Map<String, Object> params) {
+        // can be implemented 1:1 with get request.
+    }
+    private void getTransactionsOverviewHandler(final Map<String, Object> params) {
+        // can be implemented 1:1 with get request, needs some more parsing in api.
+    }
+    private void getUserAccessHandler(final Map<String, Object> params) {
+        // can be implemented 1:1 with get request.
+    }
+    private void getBankAccountAccessHandler(final Map<String, Object> params) {
+        // not yet in the system functionality, will need to be added.
+    }
 }
