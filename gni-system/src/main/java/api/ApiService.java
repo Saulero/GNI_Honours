0e025d194c463e0f5fa2515f4bc95199c67b235a
Noel Keijzer <n.keijzer@student.utwente.nl>
2017-06-04 18:55:14 +0200

    Add payFromAccount function to ApiService

--- gni-system/src/main/java/api/ApiService.java
+++ gni-system/src/main/java/api/ApiService.java
@@ -52,31 +52,31 @@ public final class ApiService {
             Map<String, Object> params = request.getNamedParams();
             CallbackBuilder callbackBuilder = CallbackBuilder.newCallbackBuilder().withStringCallback(callback);
             switch (method) {
-                case "openAccount":             openAccountHandler(params, callbackBuilder, id);
+                case "openAccount":             openAccount(params, callbackBuilder, id);
                     break;
-                case "openAdditionalAccount":   openAdditionalAccountHandler(params, callbackBuilder, id);
+                case "openAdditionalAccount":   openAdditionalAccount(params, callbackBuilder, id);
                     break;
-                case "closeAccount":            closeAccountHandler(params, callbackBuilder, id);
+                case "closeAccount":            closeAccount(params, callbackBuilder, id);
                     break;
-                case "provideAccess":           provideAccessHandler(params, callbackBuilder, id);
+                case "provideAccess":           provideAccess(params, callbackBuilder, id);
                     break;
-                case "revokeAccess":            revokeAccessHandler(params, callbackBuilder, id);
+                case "revokeAccess":            revokeAccess(params, callbackBuilder, id);
                     break;
-                case "depositIntoAccount":      depositIntoAccountHandler(params, callbackBuilder, id);
+                case "depositIntoAccount":      depositIntoAccount(params, callbackBuilder, id);
                     break;
-                case "payFromAccount":          payFromAccountHandler(params, callbackBuilder, id);
+                case "payFromAccount":          payFromAccount(params, callbackBuilder, id);
                     break;
-                case "transferMoney":           transferMoneyHandler(params, callbackBuilder, id);
+                case "transferMoney":           transferMoney(params, callbackBuilder, id);
                     break;
-                case "getAuthToken":            getAuthTokenHandler(params, callbackBuilder, id);
+                case "getAuthToken":            getAuthToken(params, callbackBuilder, id);
                     break;
-                case "getBalance":              getBalanceHandler(params, callbackBuilder, id);
+                case "getBalance":              getBalance(params, callbackBuilder, id);
                     break;
-                case "getTransactionsOverview": getTransactionsOverviewHandler(params, callbackBuilder, id);
+                case "getTransactionsOverview": getTransactionsOverview(params, callbackBuilder, id);
                     break;
-                case "getUserAccess":           getUserAccessHandler(params, callbackBuilder, id);
+                case "getUserAccess":           getUserAccess(params, callbackBuilder, id);
                     break;
-                case "getBankAccountAccess":    getBankAccountAccessHandler(params, callbackBuilder, id);
+                case "getBankAccountAccess":    getBankAccountAccess(params, callbackBuilder, id);
                     break;
                 default:                        callback.reply(new JSONRPC2Response(JSONRPC2Error.METHOD_NOT_FOUND,
                         request.getID()).toJSONString());
@@ -87,8 +87,8 @@ public final class ApiService {
         }
     }
 
-    private void openAccountHandler(final Map<String, Object> params, final CallbackBuilder callbackBuilder,
-                                    final Object id) {
+    private void openAccount(final Map<String, Object> params, final CallbackBuilder callbackBuilder,
+                             final Object id) {
         Customer customer = JSONParser.createJsonCustomer((String) params.get("initials"), (String) params.get("name"),
                 (String) params.get("surname"), (String) params.get("email"), (String) params.get("telephoneNumber"),
                 (String) params.get("address"), (String) params.get("dob"), (Long) params.get("ssn"),
@@ -103,7 +103,7 @@ public final class ApiService {
                     if (statusCode == HTTP_OK) {
                         Customer reply = jsonConverter.fromJson(JSONParser.removeEscapeCharacters(replyJson), Customer.class);
                         System.out.printf("%s Customer successfully created in the system.\n\n\n\n", PREFIX);
-                        doLogin(customer.getUsername(), customer.getPassword(),
+                        login(customer.getUsername(), customer.getPassword(),
                                 reply.getAccount().getAccountNumber(), callbackBuilder, id);
                     } else {
                         System.out.printf("%s Customer creation request failed, body: %s\n\n\n\n", PREFIX, replyJson);
@@ -112,8 +112,8 @@ public final class ApiService {
                 });
     }
 
-    private void doLogin(final String username, final String password, final String accountNumber,
-                         final CallbackBuilder callbackBuilder, final Object id) {
+    private void login(final String username, final String password, final String accountNumber,
+                       final CallbackBuilder callbackBuilder, final Object id) {
         Authentication authentication = JSONParser.createJsonAuthenticationLogin(username, password);
         Gson gson = new Gson();
         System.out.printf("%s Logging in.\n", PREFIX);
@@ -159,41 +159,47 @@ public final class ApiService {
         callbackBuilder.build().reply(response.toJSONString());
     }
 
-    private void openAdditionalAccountHandler(final Map<String, Object> params, final CallbackBuilder callbackBuilder,
-                                              final Object id) {
+    private void openAdditionalAccount(final Map<String, Object> params, final CallbackBuilder callbackBuilder,
+                                       final Object id) {
         //todo needs modifications so users loads customerData during request as we currently require customer data to be specified.
     }
 
-    private void closeAccountHandler(final Map<String, Object> params, final CallbackBuilder callbackBuilder,
-                                     final Object id) {
+    private void closeAccount(final Map<String, Object> params, final CallbackBuilder callbackBuilder,
+                              final Object id) {
         uiClient.putFormAsyncWith2Params("/services/ui/account/remove", "accountNumber",
                 params.get("iBAN"), "cookie", params.get("authToken"), (code, contentType, body) -> {
                     if (code == HTTP_OK) {
-                        System.out.printf("%s Successfully closed account %s\n\n\n\n", PREFIX, body);
-                        Map<String, Object> result = new HashMap<>();
-                        JSONRPC2Response response = new JSONRPC2Response(result, id);
-                        callbackBuilder.build().reply(response.toJSONString());
+                        sendCloseAccountCallback(callbackBuilder, id, body);
                     } else {
                         System.out.printf("%s Account closing failed.\n\n\n\n", PREFIX);
+                        //todo send back result with appropriate error
                     }
                 });
     }
 
-    private void provideAccessHandler(final Map<String, Object> params, final CallbackBuilder callbackBuilder,
-                                      final Object id) {
+    private void sendCloseAccountCallback(final CallbackBuilder callbackBuilder, final Object id,
+                                          final String replyAccountNumber) {
+        System.out.printf("%s Successfully closed account %s\n\n\n\n", PREFIX, replyAccountNumber);
+        Map<String, Object> result = new HashMap<>();
+        JSONRPC2Response response = new JSONRPC2Response(result, id);
+        callbackBuilder.build().reply(response.toJSONString());
+    }
+
+    private void provideAccess(final Map<String, Object> params, final CallbackBuilder callbackBuilder,
+                               final Object id) {
         // does an account Link to a username(so we need a conversion for this internally)
         // then performs a new pin card request for the customer with username.
     }
 
-    private void revokeAccessHandler(final Map<String, Object> params, final CallbackBuilder callbackBuilder,
-                                     final Object id) {
+    private void revokeAccess(final Map<String, Object> params, final CallbackBuilder callbackBuilder,
+                              final Object id) {
         //todo add functionality for account Link removal
         // performs an account Link removal and then removes the pincard(s) of said customer.
         // look at documentation for more specifics.
     }
 
-    private void depositIntoAccountHandler(final Map<String, Object> params, final CallbackBuilder callbackBuilder,
-                                           final Object id) {
+    private void depositIntoAccount(final Map<String, Object> params, final CallbackBuilder callbackBuilder,
+                                    final Object id) {
         PinTransaction pin = JSONParser.createJsonPinTransaction(ATMNUMBER, (String) params.get("iBAN"),
                 "", (String) params.get("pinCode"), (Long) params.get("pinCard"),
                 (Double) params.get("amount"), true);
@@ -221,32 +227,57 @@ public final class ApiService {
                     }
                 });
     }
-    private void payFromAccountHandler(final Map<String, Object> params, final CallbackBuilder callbackBuilder,
-                                       final Object id) {
-        // can be implemented 1:1 with a pin transaction.
+    private void payFromAccount(final Map<String, Object> params, final CallbackBuilder callbackBuilder,
+                                final Object id) {
+        PinTransaction pin = JSONParser.createJsonPinTransaction((String) params.get("sourceIBAN"),
+                (String) params.get("targetIBAN"), "", (String) params.get("pinCode"),
+                (Long) params.get("pinCard"), (Double) params.get("amount"), false);
+        System.out.printf("%s Sending pin transaction.\n", PREFIX);
+        pinClient.putFormAsyncWith1Param("/services/pin/transaction", "request",
+                jsonConverter.toJson(pin), (code, contentType, body) -> {
+                    if (code == HTTP_OK) {
+                        Transaction reply = jsonConverter.fromJson(JSONParser.removeEscapeCharacters(body),
+                                                                   Transaction.class);
+                        if (reply.isSuccessful() && reply.isProcessed()) {
+                            System.out.printf("%s Pin transaction successfull.\n\n\n", PREFIX);
+                            Map<String, Object> result = new HashMap<>();
+                            JSONRPC2Response response = new JSONRPC2Response(result, id);
+                            callbackBuilder.build().reply(response.toJSONString());
+                        } else if (!reply.isProcessed()) {
+                            System.out.printf("%s Pin transaction couldn't be processed.\n\n\n", PREFIX);
+                            //todo figure out how to fetch what cause the failed processing.
+                        } else {
+                            System.out.printf("%s Pin transaction was not successfull.\n\n\n", PREFIX);
+                            //todo send unsuccessfull reply, find way to fetch cause of this.
+                        }
+                    } else {
+                        System.out.printf("%s Pin transaction request failed.\n\n\n", PREFIX);
+                        //todo figure out a way to find out what made the request fail.
+                    }
+                });
     }
-    private void transferMoneyHandler(final Map<String, Object> params, final CallbackBuilder callbackBuilder,
-                                      final Object id) {
+    private void transferMoney(final Map<String, Object> params, final CallbackBuilder callbackBuilder,
+                               final Object id) {
         // can be implemented 1:1 with a normal transaction request.
     }
-    private void getAuthTokenHandler(final Map<String, Object> params, final CallbackBuilder callbackBuilder,
-                                     final Object id) {
+    private void getAuthToken(final Map<String, Object> params, final CallbackBuilder callbackBuilder,
+                              final Object id) {
         // can be implemented 1:1 with do login.
     }
-    private void getBalanceHandler(final Map<String, Object> params, final CallbackBuilder callbackBuilder,
-                                   final Object id) {
+    private void getBalance(final Map<String, Object> params, final CallbackBuilder callbackBuilder,
+                            final Object id) {
         // can be implemented 1:1 with get request.
     }
-    private void getTransactionsOverviewHandler(final Map<String, Object> params, final CallbackBuilder callbackBuilder,
-                                                final Object id) {
+    private void getTransactionsOverview(final Map<String, Object> params, final CallbackBuilder callbackBuilder,
+                                         final Object id) {
         // can be implemented 1:1 with get request, needs some more parsing in api.
     }
-    private void getUserAccessHandler(final Map<String, Object> params, final CallbackBuilder callbackBuilder,
-                                      final Object id) {
+    private void getUserAccess(final Map<String, Object> params, final CallbackBuilder callbackBuilder,
+                               final Object id) {
         // can be implemented 1:1 with get request.
     }
-    private void getBankAccountAccessHandler(final Map<String, Object> params, final CallbackBuilder callbackBuilder,
-                                             final Object id) {
+    private void getBankAccountAccess(final Map<String, Object> params, final CallbackBuilder callbackBuilder,
+                                      final Object id) {
         // not yet in the system functionality, will need to be added.
     }
 }
