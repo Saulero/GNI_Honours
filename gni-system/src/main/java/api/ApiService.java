fff19e1a7c4fa90fa19f43c13748dbd8733e4305
Noel Keijzer <n.keijzer@student.utwente.nl>
2017-06-04 17:11:18 +0200

    add closeAccountHandler implementation

--- gni-system/src/main/java/api/ApiService.java
+++ gni-system/src/main/java/api/ApiService.java
@@ -5,9 +5,7 @@ import com.thetransactioncompany.jsonrpc2.JSONRPC2Error;
 import com.thetransactioncompany.jsonrpc2.JSONRPC2ParseException;
 import com.thetransactioncompany.jsonrpc2.JSONRPC2Request;
 import com.thetransactioncompany.jsonrpc2.JSONRPC2Response;
-import databeans.Authentication;
-import databeans.Customer;
-import databeans.PinCard;
+import databeans.*;
 import io.advantageous.qbit.annotation.RequestMapping;
 import io.advantageous.qbit.annotation.RequestMethod;
 import io.advantageous.qbit.annotation.RequestParam;
@@ -30,13 +28,18 @@ import static java.net.HttpURLConnection.HTTP_OK;
 public final class ApiService {
     /** Connection to the ui service. */
     private HttpClient uiClient;
+    /** Connection to the upini service. */
+    private HttpClient pinClient;
     /** Used for json conversions. */
     private Gson jsonConverter;
     /** Prefix used when printing to indicate the message is coming from the Api Service. */
     private static final String PREFIX = "[API]                 :";
+    /** Number of the ATM system for internal use*/
+    private static final String ATMNUMBER = "NL52GNIB3676451168";
 
-    public ApiService(final int uiPort, final String uiHost) {
+    public ApiService(final int uiPort, final String uiHost, final int pinPort, final String pinHost) {
         uiClient = httpClientBuilder().setHost(uiHost).setPort(uiPort).buildAndStart();
+        pinClient = httpClientBuilder().setHost(pinHost).setPort(pinPort).buildAndStart();
         jsonConverter = new Gson();
     }
 
@@ -51,29 +54,29 @@ public final class ApiService {
             switch (method) {
                 case "openAccount":             openAccountHandler(params, callbackBuilder, id);
                     break;
-                case "openAdditionalAccount":   openAdditionalAccountHandler(params);
+                case "openAdditionalAccount":   openAdditionalAccountHandler(params, callbackBuilder, id);
                     break;
-                case "closeAccount":            closeAccountHandler(params);
+                case "closeAccount":            closeAccountHandler(params, callbackBuilder, id);
                     break;
-                case "provideAccess":           provideAccessHandler(params);
+                case "provideAccess":           provideAccessHandler(params, callbackBuilder, id);
                     break;
-                case "revokeAccess":            revokeAccessHandler(params);
+                case "revokeAccess":            revokeAccessHandler(params, callbackBuilder, id);
                     break;
-                case "depositIntoAccount":      depositIntoAccountHandler(params);
+                case "depositIntoAccount":      depositIntoAccountHandler(params, callbackBuilder, id);
                     break;
-                case "payFromAccount":          payFromAccountHandler(params);
+                case "payFromAccount":          payFromAccountHandler(params, callbackBuilder, id);
                     break;
-                case "transferMoney":           transferMoneyHandler(params);
+                case "transferMoney":           transferMoneyHandler(params, callbackBuilder, id);
                     break;
-                case "getAuthToken":            getAuthTokenHandler(params);
+                case "getAuthToken":            getAuthTokenHandler(params, callbackBuilder, id);
                     break;
-                case "getBalance":              getBalanceHandler(params);
+                case "getBalance":              getBalanceHandler(params, callbackBuilder, id);
                     break;
-                case "getTransactionsOverview": getTransactionsOverviewHandler(params);
+                case "getTransactionsOverview": getTransactionsOverviewHandler(params, callbackBuilder, id);
                     break;
-                case "getUserAccess":           getUserAccessHandler(params);
+                case "getUserAccess":           getUserAccessHandler(params, callbackBuilder, id);
                     break;
-                case "getBankAccountAccess":    getBankAccountAccessHandler(params);
+                case "getBankAccountAccess":    getBankAccountAccessHandler(params, callbackBuilder, id);
                     break;
                 default:                        callback.reply(new JSONRPC2Response(JSONRPC2Error.METHOD_NOT_FOUND,
                         request.getID()).toJSONString());
@@ -148,7 +151,7 @@ public final class ApiService {
 
     private void sendOpenAccountCallback(final CallbackBuilder callbackBuilder, final String accountNumber,
                                          final Long cardNumber, final String pinCode, final Object id) {
-        Map<String, Object> result = new HashMap<String, Object>();
+        Map<String, Object> result = new HashMap<>();
         result.put("iBAN", accountNumber);
         result.put("pinCard", cardNumber);
         result.put("pinCode", pinCode);
@@ -156,51 +159,69 @@ public final class ApiService {
         callbackBuilder.build().reply(response.toJSONString());
     }
 
-    private void openAdditionalAccountHandler(final Map<String, Object> params) {
+    private void openAdditionalAccountHandler(final Map<String, Object> params, final CallbackBuilder callbackBuilder,
+                                              final Object id) {
         //todo needs modifications so users loads customerData during request as we currently require customer data to be specified.
     }
-    private void closeAccountHandler(final Map<String, Object> params) {
+
+    private void closeAccountHandler(final Map<String, Object> params, final CallbackBuilder callbackBuilder,
+                                     final Object id) {
         uiClient.putFormAsyncWith2Params("/services/ui/account/remove", "accountNumber",
                 params.get("iBAN"), "cookie", params.get("authToken"), (code, contentType, body) -> {
                     if (code == HTTP_OK) {
                         System.out.printf("%s Successfully closed account %s\n\n\n\n", PREFIX, body);
-                        //todo handle response
+                        Map<String, Object> result = new HashMap<>();
+                        JSONRPC2Response response = new JSONRPC2Response(result, id);
+                        callbackBuilder.build().reply(response.toJSONString());
                     } else {
                         System.out.printf("%s Account closing failed.\n\n\n\n", PREFIX);
                     }
                 });
     }
-    private void provideAccessHandler(final Map<String, Object> params) {
+
+    private void provideAccessHandler(final Map<String, Object> params, final CallbackBuilder callbackBuilder,
+                                      final Object id) {
         // does an account Link to a username(so we need a conversion for this internally)
         // then performs a new pin card request for the customer with username.
     }
-    private void revokeAccessHandler(final Map<String, Object> params) {
+
+    private void revokeAccessHandler(final Map<String, Object> params, final CallbackBuilder callbackBuilder,
+                                     final Object id) {
         //todo add functionality for account Link removal
         // performs an account Link removal and then removes the pincard(s) of said customer.
         // look at documentation for more specifics.
     }
-    private void depositIntoAccountHandler(final Map<String, Object> params) {
+
+    private void depositIntoAccountHandler(final Map<String, Object> params, final CallbackBuilder callbackBuilder,
+                                           final Object id) {
+
+    }
+    private void payFromAccountHandler(final Map<String, Object> params, final CallbackBuilder callbackBuilder,
+                                       final Object id) {
         // can be implemented 1:1 with a pin transaction.
     }
-    private void payFromAccountHandler(final Map<String, Object> params) {
-        // can be implemented 1:1 with a pin transaction.
-    }
-    private void transferMoneyHandler(final Map<String, Object> params) {
+    private void transferMoneyHandler(final Map<String, Object> params, final CallbackBuilder callbackBuilder,
+                                      final Object id) {
         // can be implemented 1:1 with a normal transaction request.
     }
-    private void getAuthTokenHandler(final Map<String, Object> params) {
+    private void getAuthTokenHandler(final Map<String, Object> params, final CallbackBuilder callbackBuilder,
+                                     final Object id) {
         // can be implemented 1:1 with do login.
     }
-    private void getBalanceHandler(final Map<String, Object> params) {
+    private void getBalanceHandler(final Map<String, Object> params, final CallbackBuilder callbackBuilder,
+                                   final Object id) {
         // can be implemented 1:1 with get request.
     }
-    private void getTransactionsOverviewHandler(final Map<String, Object> params) {
+    private void getTransactionsOverviewHandler(final Map<String, Object> params, final CallbackBuilder callbackBuilder,
+                                                final Object id) {
         // can be implemented 1:1 with get request, needs some more parsing in api.
     }
-    private void getUserAccessHandler(final Map<String, Object> params) {
+    private void getUserAccessHandler(final Map<String, Object> params, final CallbackBuilder callbackBuilder,
+                                      final Object id) {
         // can be implemented 1:1 with get request.
     }
-    private void getBankAccountAccessHandler(final Map<String, Object> params) {
+    private void getBankAccountAccessHandler(final Map<String, Object> params, final CallbackBuilder callbackBuilder,
+                                             final Object id) {
         // not yet in the system functionality, will need to be added.
     }
 }
